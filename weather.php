3689342a7bafa32624922a541c27fda61986b60b
Dark Tornado <dt3141592@naver.com>
2021-09-07 10:18:31 +0900

    날씨 UI 디자인 변경 예정 대응

--- weather.php
+++ weather.php
@@ -2,11 +2,11 @@
 header('Content-Type: application/json; charset=utf-8');
 clearstatcache();
 $params = array();
-$params_raw = file_get_contents("php://input");
+$params_raw = file_get_contents('php://input');
 parse_str($params_raw, $params);
 
 $key = '안알랴줌';
-$url = 'http://api.openweathermap.org/data/2.5/onecall?lat='.$params['x'].'&lon='.$params['y'].'&exclude=current,minutely,hourly&units=metric&APPID='.$key;
+$url = 'http://api.openweathermap.org/data/2.5/onecall?lat='.$params['x'].'&lon='.$params['y'].'&exclude=minutely,hourly&units=metric&APPID='.$key;
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -14,25 +14,37 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
 
-$data = json_decode($response, true);
-$data = $data['daily'];
-$days = array("오늘", "내일", "모래");
+$data0 = json_decode($response, true);
 
-echo '[';
+echo '{';
+
+/* 현재 날씨 */
+$data = $data0['current'];
+echo '"current":{';
+echo '"status":"'.$data['weather'][0]['main'].'",';
+echo '"temp":"'.$data['temp'].'℃",';
+echo '"temp_wind":"'.$data['feels_like'].'℃",';
+echo '"hum":"'.$data['humidity'].'%",';
+echo '"wind_speed":"'.$data['wind_speed'].'m/s",';
+echo '"wind_dir":"'.deg2dir($data['wind_deg']).'"';
+echo '},';
+
+/* 주간 날씨 */
+$data = $data0['daily'];
+$days = array('오늘', '내일', '모래', '글피', '5일 뒤', '6일 뒤');
+echo '"weekly":[';
 for($n = 0; $n < count($days); $n++) {
     if ($n > 0) echo ",";
     echo '{';
     echo '"date":"'.$days[$n].'",';
     echo '"status":"'.$data[$n]['weather'][0]['main'].'",';
-    echo '"icon":"https://openweathermap.org/img/wn/'.$data[$n]['weather'][0]['icon'].'@4x.png",';
     echo '"temp_min":"'.$data[$n]['temp']['min'].'℃",';
     echo '"temp_max":"'.$data[$n]['temp']['max'].'℃",';
-    echo '"hum":"'.$data[$n]['humidity'].'%",';
-    echo '"wind_speed":"'.$data[$n]['wind_speed'].'m/s",';
-    echo '"wind_dir":"'.deg2dir($data['wind_deg']).'"';
     echo '}';
 }
 echo ']';
+
+echo '}';
 
 function deg2dir($deg){
     $dirs = array('북', '북동', '동', '남동', '남', '남서', '서', '북서', '북');
